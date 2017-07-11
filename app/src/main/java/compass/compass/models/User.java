0925e39048a0b8507d3a431de0a67ed326587cf0
Isabella Garcia Camargo <icamargo@fb.com>
2017-07-11 15:32:58 -0700

    added models

---
+++ app/src/main/java/compass/compass/models/User.java
@@ -0,0 +1,33 @@
+package compass.compass.models;
+
+/**
+ * Created by icamargo on 7/11/17.
+ */
+
+
+public class User {
+    public String name;
+    public String email;
+    public String gender;
+    //data class for geographic location: latitude, lognitude, timestamp, and other info
+    //public Location location;
+
+
+    //****INFORMATION TO UNCOMMENT AS WE ADD EXTRA FEATURES *****//
+//    public ArrayList<Event> eventList;
+//    public DrinkInfo drinkInfo;
+//    //would make a drinkInfo class that takes in height, weight, etc. and comes up with formula
+//    //or something along those lines
+//    public PriorityInfo priorityInfo;
+//    //another module to decompose, takes in a string message, time, etc. for priority to keep trakc
+//    //of for the user
+//    public String school;
+
+    public User(String nameInfo, String emailInfo, String genderInfo){
+        name = nameInfo;
+        email = emailInfo;
+        gender = genderInfo;
+    }
+
+
+}
