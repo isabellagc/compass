ed816b02ffbcc915aba0c0ca0c3ffa588feeeb83
Amulya Musipatla <amusipatla@fb.com>
2017-07-13 13:41:02 -0700

    starting w messaging

---
+++ app/src/main/java/compass/compass/MyFirebaseInstanceIDService.java
@@ -0,0 +1,32 @@
+package compass.compass;
+
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
+
+/**
+ * Created by amusipatla on 7/12/17.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Nullable
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+    }
+}
