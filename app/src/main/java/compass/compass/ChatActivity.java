d30ed3fccab80d9c3885b61aa81ad954cc4b2e9e
brucegatete <bgatete@college.harvard.edu>
2017-07-21 14:23:34 -0700

    notifications comment added

--- app/src/main/java/compass/compass/ChatActivity.java
+++ app/src/main/java/compass/compass/ChatActivity.java
@@ -135,8 +135,7 @@ public class ChatActivity extends FragmentActivity implements OnMapReadyCallback
                 message.setSender(currentProfile.name);
                 message.setTime((new Date().getTime()));
                 etMessage.getText().clear();
-
-
+                //Notification sender,When the message is sent
                 mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(ChatActivity.this)
                         .setSmallIcon(R.drawable.ic_need_help)
