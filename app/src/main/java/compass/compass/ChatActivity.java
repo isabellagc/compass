1e6e275715e40bfafcc7286f1c3901ad1aafd6aa
Amulya Musipatla <amusipatla@fb.com>
2017-07-24 12:09:11 -0700

    only send notifications for messages to people who are out

--- app/src/main/java/compass/compass/ChatActivity.java
+++ app/src/main/java/compass/compass/ChatActivity.java
@@ -50,6 +50,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -119,12 +120,37 @@ public class ChatActivity extends FragmentActivity implements OnMapReadyCallback
         layoutManager.setStackFromEnd(true);
         rvChat.setLayoutManager(layoutManager);
 
+
+        //mDatabase.child("Events").child(eventId).child("Members").addValueEventListener()
+
         //get other members of group
         mDatabase.child("Events").child(eventId).child("Members").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Map otherPeople = (Map) dataSnapshot.getValue();
                 Set<String> people= otherPeople.keySet();
+                people.remove(currentProfile.name.replaceAll(" ", ""));
+                members = (String[]) people.toArray(new String[people.size()]);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //get other members of group
+        mDatabase.child("Events").child(eventId).child("Members").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map otherPeople = (Map) dataSnapshot.getValue();
+                Set<String> people= new HashSet<String>();
+                for(Object s : otherPeople.keySet()) {
+                    String temp = s.toString().replaceAll(" ", "");
+                    if((otherPeople.get(s).toString().contentEquals("out"))){
+                        people.add(temp);
+                    }
+                }
                 people.remove(currentProfile.name.replaceAll(" ", ""));
                 members = (String[]) people.toArray(new String[people.size()]);
             }
@@ -167,49 +193,10 @@ public class ChatActivity extends FragmentActivity implements OnMapReadyCallback
                         rvChat.smoothScrollToPosition(mAdapter.getItemCount());
                     }
                 });
-                //send notification to the user
 
+                //send notification to the user
                 sendNotificationToUser(members, message);
 
-
-                // notification manager to send notification when the message is sent
-//
-//                Intent intent = new Intent(ChatActivity.this, MainActivity.class);
-//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-//                PendingIntent pendingIntent = PendingIntent.getActivity(ChatActivity.this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-//                Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//                // notification manager to send notification when the message is sent
-//                mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-//                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(ChatActivity.this)
-//                        .setSmallIcon(R.drawable.ic_need_help)
-//                        .setContentTitle("New Message from " + message.getSender())
-//                        .setContentText(message.getText())
-//                        .setOnlyAlertOnce(true)
-//                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-//                        .setSound(notificationSound)
-//                        .setContentIntent(pendingIntent);
-//                mBuilder.setAutoCancel(true);
-//                mBuilder.setLocalOnly(false);
-//
-//                mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//                mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-
-//                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
-//                mBuilder.setAutoCancel(true);
-//                mBuilder.setLocalOnly(false);
-////
-////
-//                mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-//                mDatabase.child("messages").child(eventId).push().setValue(message);
-//                mAdapter.notifyDataSetChanged();
-//                rvChat.post( new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        rvChat.smoothScrollToPosition(mAdapter.getItemCount());
-//                    }
-//                });
             }
         });
 
