819bd076ec8308c729179fecc0daf852f04dcdcf
brucegatete <bgatete@college.harvard.edu>
2017-08-03 16:53:57 -0700

    Near you tab renamed

--- app/src/main/java/compass/compass/fragments/ResourcePagerAdapter.java
+++ app/src/main/java/compass/compass/fragments/ResourcePagerAdapter.java
@@ -11,7 +11,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class ResourcePagerAdapter extends FragmentPagerAdapter{
 
-    private String tabTitles [] = new String[] {"School Help", "Location Help"};
+    private String tabTitles [] = new String[] {"School Help", "Near You"};
     private Context context;
     public ResourceSchoolFragment resourceSchoolFragment;
     public  ResourceLocationFragment resourceLocationFragment;
